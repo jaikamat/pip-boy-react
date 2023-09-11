3b29f91062612bb08bc4925e586852bdedefea8d
jaikamat <jaipkamat@gmail.com>
2023-09-11 17:14:54 -0400

    build finagling

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,8 @@ function App() {
   const [activeTab, setActiveTab] = useState<string>(Tabs.STAT);
 
   return (
-    <div className="container glow-text crt-effect">
+    // <div className="container glow-text crt-effect">
+    <div className="container">
       <div className="container-item">
         <Header onChange={(tab) => setActiveTab(tab)} initialTab={activeTab} />
       </div>
