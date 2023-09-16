80cf65540c24161e03e1719cd221f5c66d39a75d
jaikamat <jaipkamat@gmail.com>
2023-09-15 21:28:34 -0400

    chore(pbr): trying leaflet

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from "react";
-import { Graticule, Mercator } from "@visx/geo";
+import { useEffect } from "react";
 import * as topojson from "topojson-client";
 import topology from "./world-topo.json";
+import { MapContainer, GeoJSON, useMap } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
 interface FeatureShape {
   type: "Feature";
@@ -16,57 +17,20 @@ const world = topojson.feature(topology, topology.objects.units) as {
   features: FeatureShape[];
 };
 
-const Map: React.FC = () => {
-  const width: number = 799;
-  const height: number = 479;
-  const centerX = width / 2;
-  const centerY = height / 2;
-  const initialScale = (width / 630) * 100;
-
-  const [offsetX, setOffsetX] = useState<number>(0);
-  const [offsetY, setOffsetY] = useState<number>(0);
-  const [scale, setScale] = useState<number>(initialScale);
-
-  const adjustOffsetsForZoom = (zoomFactor: number) => {
-    // Calculate zoom focal point. This is the center of the current SVG content's visible part.
-    const focalX = offsetX;
-    const focalY = offsetY - 50;
-
-    // Adjust the offsets based on the zoom factor and the focal point
-    setOffsetX(
-      (prevOffset) => prevOffset + (focalX - prevOffset) * (1 - zoomFactor)
-    );
-    setOffsetY(
-      (prevOffset) => prevOffset + (focalY - prevOffset) * (1 - zoomFactor)
-    );
-  };
+const FocusOnLoad = () => {
+  const map = useMap();
 
   const handleKeyDown = (event: KeyboardEvent) => {
-    const PAN_AMOUNT = 30;
-    const ZOOM_AMOUNT = 1.5; // This can be adjusted. Greater than 1 for zooming in, less than 1 for zooming out
-
     switch (event.key) {
       case "ArrowUp":
         if (event.metaKey) {
-          setScale((prevScale) => prevScale * ZOOM_AMOUNT);
-          adjustOffsetsForZoom(ZOOM_AMOUNT);
-        } else {
-          setOffsetY((prevState) => prevState + PAN_AMOUNT);
+          map.zoomIn();
         }
         break;
       case "ArrowDown":
         if (event.metaKey) {
-          setScale((prevScale) => prevScale / ZOOM_AMOUNT);
-          adjustOffsetsForZoom(1 / ZOOM_AMOUNT);
-        } else {
-          setOffsetY((prevState) => prevState - PAN_AMOUNT);
+          map.zoomOut();
         }
-        break;
-      case "ArrowLeft":
-        setOffsetX((prevState) => prevState + PAN_AMOUNT);
-        break;
-      case "ArrowRight":
-        setOffsetX((prevState) => prevState - PAN_AMOUNT);
         break;
     }
   };
@@ -78,33 +42,35 @@ const Map: React.FC = () => {
     };
   }, []);
 
+  useEffect(() => {
+    map.whenReady(() => {
+      const mapContainer = map.getContainer();
+      mapContainer.focus();
+    });
+  }, [map]);
+
+  return null;
+};
+
+const Map = () => {
   return (
-    <svg width={width} height={height}>
-      <Mercator
-        data={world.features}
-        scale={scale}
-        translate={[centerX + offsetX, centerY + 50 + offsetY]}
-      >
-        {(mercator) => (
-          <g>
-            {mercator.features.map(({ feature, path }, i) => (
-              <path
-                key={`map-feature-${i}`}
-                d={path || ""}
-                stroke="#1adc09"
-                strokeWidth={2}
-                // fill="url(#transparentToGreen)"
-              />
-            ))}
-            <Graticule
-              graticule={(g) => mercator.path(g) || ""}
-              stroke="rgba(26, 220, 9, 0.2)"
-              strokeWidth={2}
-            />
-          </g>
-        )}
-      </Mercator>
-    </svg>
+    <MapContainer
+      preferCanvas
+      zoomControl={false}
+      attributionControl={false}
+      center={[51.505, -0.09]}
+      zoom={3}
+      style={{
+        width: "100%",
+        height: "100%",
+        // zIndex: 200,
+        backgroundColor: "black",
+        outline: "none",
+      }}
+    >
+      <FocusOnLoad />
+      <GeoJSON data={world} pathOptions={{ color: "#1adc09" }} />
+    </MapContainer>
   );
 };
 
