eb8e4f7bef8355d6a7ddd224da6dd625ba16a857
jaikamat <jaipkamat@gmail.com>
2023-09-26 15:44:39 -0400

    chore(pbr): refine map markers

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -5,15 +5,26 @@ import "leaflet/dist/leaflet.css";
 import leaflet from "leaflet";
 import "./Map.css";
 import MarkerClusterGroup from "react-leaflet-cluster";
+import "leaflet.markercluster/dist/MarkerCluster.css";
+import "leaflet.markercluster/dist/MarkerCluster.Default.css";
 
 // @ts-ignore
 const createCustomIcon = (text) => {
   return leaflet.divIcon({
-    className: "custom-icon",
-    html: `<div class="custom-icon-text">${text}</div>`,
-    iconSize: [15, 15], // size of the icon
+    className: "custom-marker",
+    html: `<div class="custom-marker-text">${text}</div>`,
+    iconSize: [8, 8],
     iconAnchor: [15, 15], // point of the icon which will correspond to marker's location
     popupAnchor: [0, -15], // point from which the popup should open relative to the iconAnchor
+  });
+};
+
+// @ts-ignore
+const createClusterCustomIcon = function (cluster) {
+  return leaflet.divIcon({
+    html: `<span>${cluster.getChildCount()}</span>`,
+    className: "custom-marker-cluster",
+    iconSize: [25, 25],
   });
 };
 
@@ -67,9 +78,10 @@ const Map = () => {
       preferCanvas
       zoomControl={false}
       attributionControl={false}
+      // Center on the Marriott
       center={[33.761585, -84.385612]}
       zoom={17}
-      maxZoom={30}
+      maxZoom={20}
       style={{
         width: "100%",
         height: "100%",
@@ -79,6 +91,7 @@ const Map = () => {
     >
       <FocusOnLoad />
       <MarkerClusterGroup
+        iconCreateFunction={createClusterCustomIcon}
         animate={false}
         removeOutsideVisibleBounds
         disableClusteringAtZoom={19}
