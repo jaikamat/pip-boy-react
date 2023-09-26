8d2a9e5068a13e97ea0af8bb81d8976fb38eb6d5
jaikamat <jaipkamat@gmail.com>
2023-09-26 17:47:31 -0400

    chore(pbr): more map adventures

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,12 +1,18 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import dragonCon from "./atlanta-dragoncon.json";
-import { MapContainer, GeoJSON, useMap, GeoJSONProps } from "react-leaflet";
+import {
+  MapContainer,
+  GeoJSON,
+  useMap,
+  GeoJSONProps,
+  Marker,
+} from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import leaflet from "leaflet";
+import leaflet, { LatLng } from "leaflet";
 import "./Map.css";
 import MarkerClusterGroup from "react-leaflet-cluster";
-import "leaflet.markercluster/dist/MarkerCluster.css";
-import "leaflet.markercluster/dist/MarkerCluster.Default.css";
+import { LatLngExpression } from "leaflet";
+import { DivIcon } from "leaflet";
 
 // @ts-ignore
 const createCustomIcon = (text) => {
@@ -14,6 +20,17 @@ const createCustomIcon = (text) => {
     className: "custom-marker",
     html: `<div class="custom-marker-text">${text}</div>`,
     iconSize: [8, 8],
+    iconAnchor: [15, 15], // point of the icon which will correspond to marker's location
+    popupAnchor: [0, -15], // point from which the popup should open relative to the iconAnchor
+  });
+};
+
+// @ts-ignore
+const createCustomStreetMarker = (text) => {
+  return leaflet.divIcon({
+    className: "custom-street-marker",
+    html: `<div class="custom-street-marker-text">${text}</div>`,
+    iconSize: [30, 30],
     iconAnchor: [15, 15], // point of the icon which will correspond to marker's location
     popupAnchor: [0, -15], // point from which the popup should open relative to the iconAnchor
   });
@@ -28,13 +45,14 @@ const createClusterCustomIcon = function (cluster) {
   });
 };
 
-// @ts-ignore
-const pointToLayer = (feature, latlng) => {
-  if (!feature.properties.name) return;
-
-  const icon = createCustomIcon(feature.properties.name);
-  return leaflet.marker(latlng, { icon });
-};
+function getCenterOfLineString(lineString: any): [number, number] {
+  const latlngs = lineString.coordinates.map((coord: any) => [
+    coord[1],
+    coord[0],
+  ]);
+  const middleIndex = Math.floor(latlngs.length / 2);
+  return latlngs[middleIndex];
+}
 
 const FocusOnLoad = () => {
   const map = useMap();
@@ -71,8 +89,63 @@ const FocusOnLoad = () => {
   return null;
 };
 
-// TODO: Gotta see how we can get the hotel names in there, for some reason they are not visible
+type MarkerData = {
+  position: LatLngExpression;
+  icon: DivIcon;
+};
+
+const GeoJsonDisplay = ({
+  addMarkers,
+}: {
+  addMarkers: ([markers]: Array<MarkerData>) => void;
+}) => {
+  return (
+    <GeoJSON
+      data={dragonCon as GeoJSONProps["data"]}
+      pathOptions={{ color: "darkgreen" }}
+      // TODO: this doesn't like "undefined" as a return value
+      // @ts-ignore
+      pointToLayer={(feature, latlng) => {
+        if (!feature.properties.name) return;
+
+        const icon = createCustomIcon(feature.properties.name);
+        return leaflet.marker(latlng, { icon });
+      }}
+      onEachFeature={(feature, layer) => {
+        let markerData: MarkerData | undefined;
+
+        // TODO: how to orient the strings along their paths?
+        // if (feature.geometry.type === "LineString" && feature.properties.name) {
+        //   const center = getCenterOfLineString(feature.geometry);
+        //   const icon = createCustomStreetMarker(feature.properties.name);
+        //   markerData = { position: center, icon };
+        // }
+
+        if (feature.geometry.type === "Polygon" && feature.properties.name) {
+          // @ts-ignore
+          const centroid = layer.getBounds().getCenter();
+          const icon = createCustomIcon(feature.properties.name);
+          markerData = { position: centroid, icon };
+        }
+
+        if (markerData) {
+          addMarkers([markerData]); // Add the marker data to the state
+        }
+      }}
+    />
+  );
+};
+
 const Map = () => {
+  const [markerDataList, setMarkerDataList] = useState<Array<MarkerData>>([]);
+
+  const addMarkers = (newMarkerData: Array<MarkerData>) => {
+    setMarkerDataList((prevMarkerDataList) => [
+      ...prevMarkerDataList,
+      ...newMarkerData,
+    ]);
+  };
+
   return (
     <MapContainer
       preferCanvas
@@ -91,17 +164,20 @@ const Map = () => {
     >
       <FocusOnLoad />
       <MarkerClusterGroup
+        chunkedLoading
         iconCreateFunction={createClusterCustomIcon}
         animate={false}
         removeOutsideVisibleBounds
         disableClusteringAtZoom={19}
       >
-        <GeoJSON
-          data={dragonCon as GeoJSONProps["data"]}
-          pathOptions={{ color: "darkgreen" }}
-          // @ts-ignore
-          pointToLayer={pointToLayer}
-        />
+        {markerDataList.map((markerData, index) => (
+          <Marker
+            key={index}
+            position={markerData.position}
+            icon={markerData.icon}
+          />
+        ))}
+        <GeoJsonDisplay addMarkers={addMarkers} />
       </MarkerClusterGroup>
     </MapContainer>
   );
